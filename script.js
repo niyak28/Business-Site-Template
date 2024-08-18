be6fccc995122f195d44015519548c90a6a94340
Niya <149212358+niyak28@users.noreply.github.com>
2024-08-18 01:46:53 +0000

    Finished home page and created contact page file + fixed header centering

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // change image on home page depending on screen size //
 if (window.innerWidth < window.innerHeight) {
   document.getElementById("checkSize").style.width = "200px";
-  document.getElementById("checkSize").style.height = "200px";
+  document.getElementById("checkSize").style.height = "fit-content";
   document.getElementById("checkSize").style.marginLeft = "65px";
 }
 else if (window.innerWidth > window.innerHeight) {
   document.getElementById("checkSize").style.width = "275px";
-  document.getElementById("checkSize").style.height = "275px";
+  document.getElementById("checkSize").style.height = "fit-content";
   document.getElementById("checkSize").style.marginLeft = "120px";
   document.getElementById("small").style.width = "65%";
+  document.getElementById("footerText").style.marginLeft = "30%";
+  document.getElementById("footer").style.height = "7%";
+  document.getElementById("footer").style.backgroundColor = "pink";
 }
